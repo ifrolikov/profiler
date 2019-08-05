00a2adb5ce26b7c90e05ab7984ffbeb191f75675
frolikov <frolikov@tutu.ru>
2019-08-05 18:45:59 +0300

    init

---
+++ Profiler.php
@@ -0,0 +1,69 @@
+<?php
+declare(strict_types=1);
+
+namespace Profiler;
+
+use Psr\Log\LoggerInterface;
+
+class Profiler
+{
+    private static $instance;
+    
+    private $tree = [];
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+    
+    public static function instance(?LoggerInterface $logger): Profiler
+    {
+        if (empty(static::$instance)) {
+            static::$instance = new Profiler($logger);
+        }
+        
+        return static::$instance;
+    }
+    
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+    
+    public function add(string $key): void
+    {
+        $start = microtime(true);
+        $fullKey = [];
+        foreach ($this->tree as $element) {
+            if (empty($element['stop'])) {
+                $fullKey[] = $element['key'];
+            }
+        }
+        $this->tree[] = [
+            'key'     => $key,
+            'fullKey' => implode('.', $fullKey),
+            'start'   => $start
+        ];
+    }
+    
+    public function stop(): string
+    {
+        $time = microtime(true);
+        $index = count($this->tree) - 1;
+        $this->tree[$index]['stop'] = $time;
+        return $this->tree[$index]['fullKey'];
+    }
+    
+    public function __destruct()
+    {
+        $log = [];
+        $stopTime = microtime(true);
+        foreach ($this->tree as $element) {
+            $elementStopTime = $element['stop'] ?? $stopTime;
+            $log[] = [
+                'key'      => $element['fullKey'],
+                'duration' => round(100 * ($element['start'] - $elementStopTime), 3)
+            ];
+        }
+        $this->logger->debug(json_encode($log, JSON_PRETTY_PRINT + JSON_UNESCAPED_UNICODE));
+    }
+}
